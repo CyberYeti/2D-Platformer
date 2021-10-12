e2c522cb5f22ab61b45e13a76b59826096d0cf0e
Yit-Meng Chin <yitmengchin@gmail.com>
2021-10-11 22:51:06 -0700

    Horizontal Movement
    +Basic horizontal movement

---
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float horizontalSpeed = 3;
+
+    private Rigidbody2D rb;
+    private float horizontalValue;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalValue = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Move(horizontalValue);
+    }
+
+    private void Move(float _horizontalVelocity)
+    {
+        float newHorizontalVelocity = _horizontalVelocity * horizontalSpeed * Time.deltaTime * 100;
+        Vector2 newVelocity = new Vector2(newHorizontalVelocity, rb.velocity.y);
+        rb.velocity = newVelocity;
+    }
+}

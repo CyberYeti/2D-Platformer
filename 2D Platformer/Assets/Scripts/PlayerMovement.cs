2bbb8a753ab5a981b3f3bab5463f0c4d91751b86
Yit-Meng Chin <yitmengchin@gmail.com>
2021-11-11 14:12:06 -0800

    Crouch code
    + Crouch changes collider and speed
    + Cannot un-crouch when ground is above you
    
    - No animations
    - Bug collider doesn't un-crouch until touching ground

--- 2D Platformer/Assets/Scripts/PlayerMovement.cs
+++ 2D Platformer/Assets/Scripts/PlayerMovement.cs
@@ -6,9 +6,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float horizontalSpeed = 3;
     [SerializeField] float runSpeedMultiplier = 2f;
+    [SerializeField] float crouchSpeedMultiplier = 0.5f;
     [SerializeField] float jumpPower = 100f;
     [SerializeField] Transform groundCheckCollider;
+    [SerializeField] Transform overheadCheckCollider;
     [SerializeField] LayerMask groundLayer;
+    [SerializeField] CapsuleCollider2D standingCollider;
     
     private Rigidbody2D rb;
     private Animator animator;
@@ -16,9 +19,11 @@ public class PlayerMovement : MonoBehaviour
     private bool isGrounded = false;
     private bool isFacingRight = true;
     private bool isRunning = false;
-    private bool jumpFlag = false;
+    private bool jump = false;
+    private bool isCrouching = false;
 
     const float GROUND_CHECK_RADIUS = 0.2f;
+    const float OVERHEAD_CHECK_RADIUS = 0.2f;
 
     void Awake()
     {
@@ -31,29 +36,38 @@ public class PlayerMovement : MonoBehaviour
         //Get Horizontal Value
         horizontalValue = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.LeftControl)) //LShift down enable running
+        if (Input.GetKeyDown(KeyCode.LeftControl)) //LControl down enable running
         {
             isRunning = true;
         }
-        else if (Input.GetKeyUp(KeyCode.LeftControl)) //LShift up disable running
+        else if (Input.GetKeyUp(KeyCode.LeftControl)) //LControl up disable running
         {
             isRunning = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)) //Space Pressed, Jump
+        if (Input.GetButtonDown("Jump")) //Space Pressed, Jump
         {
-            jumpFlag = true;
+            jump = true;
         }
-        else if (Input.GetKeyUp(KeyCode.Space)) //Space Released, Stop Jump
+        else if (Input.GetButtonUp("Jump")) //Space Released, Stop Jump
         {
-            jumpFlag = false;
+            jump = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift)) //LShift Pressed, Crouch
+        {
+            isCrouching = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift)) //LShift Released, Stop Crouch
+        {
+            isCrouching = false;
         }
     }
 
     private void FixedUpdate()
     {
         GroundCheck();
-        Move(horizontalValue, jumpFlag);
+        Move(horizontalValue, jump, isCrouching);
     }
 
     private void GroundCheck()
@@ -67,23 +81,63 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void Move(float _horizontalDirection, bool _jumpFlag)
+    private void Move(float _horizontalDirection, bool _jumpFlag, bool _crouchFlag)
     {
-        // if player is grounded and is pressing space jump
-        if (isGrounded && _jumpFlag)
+        #region Jump And Crouch
+        //If we are crouching and uncrouch, Check above player head.
+        //If there a ground layer item above us, don't uncrouch
+        //Else uncrouch
+        if (!_crouchFlag)
         {
-            isGrounded = false;
-            _jumpFlag = false;
-            rb.AddForce(new Vector2(0, jumpPower));
+            print("checking Overhead");
+            print(Physics2D.OverlapCircle(overheadCheckCollider.transform.position, OVERHEAD_CHECK_RADIUS, groundLayer));
+            if (Physics2D.OverlapCircle(overheadCheckCollider.transform.position, OVERHEAD_CHECK_RADIUS, groundLayer) != null)
+            {
+                _crouchFlag = true;
+            }
+            print(_crouchFlag);
         }
+
+        if (isGrounded)
+        {
+            //if we press crouch, disable the standing collider, reduce speed and add crouching animation
+            //if we disable crouch, enable the standing collider, resume original speed and stop crouching animation
+            standingCollider.enabled = !_crouchFlag;
+            if (_crouchFlag)
+            {
+
+            }
+            else
+            {
+
+            }
+
+            // if player is grounded and is pressing space jump
+            if (_jumpFlag)
+            {
+                isGrounded = false;
+                _jumpFlag = false;
+                rb.AddForce(new Vector2(0, jumpPower));
+            }
+            
+        }
+
+        #endregion
 
         #region Move And Run
         float newHorizontalVelocity = _horizontalDirection * horizontalSpeed * Time.fixedDeltaTime * 100;
-        //if you are running multiply speed by runSpeedMultiplier
-        if (isRunning)
+        //if you are crouching multiply speed by crouchSpeedMultiplier(slower)
+        if (_crouchFlag)
+        {
+            newHorizontalVelocity *= crouchSpeedMultiplier;
+        }
+        
+        //if you are running multiply speed by runSpeedMultiplier (higher)
+        else if (isRunning)
         {
             newHorizontalVelocity *= runSpeedMultiplier;
         }
+        
         Vector2 newVelocity = new Vector2(newHorizontalVelocity, rb.velocity.y);
         rb.velocity = newVelocity;
 
